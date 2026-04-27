7cedc932d6bee06fcf4c141c4efcfe97d30c6316
Pratyush439 <156572277+Pratyush439@users.noreply.github.com>
2026-04-27 21:37:20 +0530

    Update infer.py

--- maskplace/infer.py
+++ maskplace/infer.py
@@ -10,10 +10,10 @@ from PPO2 import PPO
 
 
 # -------------------- CONFIG --------------------
-MODEL_PATH = "/kaggle/working/Macro_Placement_ML_for_EDA/maskplace/model_best_adaptec1.pth"
+MODEL_PATH = "/kaggle/working/Macro_Placement_ML_for_EDA/maskplace/model_best_adaptec2.pth"
 PPO_PATH = "/kaggle/working/Macro_Placement_ML_for_EDA/maskplace/model/pretrained_model.pkl"   # change if needed
 
-BENCHMARK = "adaptec1"
+BENCHMARK = "adaptec2"
 GRID = 224
 NUM_SAMPLES = 50   # number of inference runs
 
